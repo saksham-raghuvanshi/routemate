c2b6bfadb4cd4047c23550354b007a3fb362dd16
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-08 12:13:38 +0530

    React Router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import ProductDetails from "./components/ProductDetails";
+import ProductLists from "./components/ProductLists";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>Header</header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<ProductLists />} />
+        <Route path="/products/1001" element={<ProductDetails />} />
+      </Routes>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
